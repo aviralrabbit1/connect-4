f16ff6006c9a3c98a2f148528fc758afe04e702f
aviralrabbit1 <aviral.robin@gmail.com>
2023-06-30 05:25:51 +0530

    dynamically coloring circles using parameter color

--- src/Components/Gameboard.js
+++ src/Components/Gameboard.js
@@ -4,35 +4,35 @@ import Gamecircle from './Gamecircle'
 function Gameboard() {
   return (
     <div>
-        <Gamecircle id ={1} value={7} >
+        <Gamecircle id ={1} color="red"  >
             <span style={{color: 'red'}} >Red</span>
         </Gamecircle>
         
-        <Gamecircle id ={2} value={7} >
+        <Gamecircle id ={2} color="blue"  >
             <span style={{color: 'blue'}} >Blue</span>
         </Gamecircle>
         
-        <Gamecircle id ={3} value={7} >
+        <Gamecircle id ={3} color="red"  >
             <span style={{color: 'red'}} >Red</span>
         </Gamecircle>
 
-        <Gamecircle id ={4} value={7} >
+        <Gamecircle id ={4} color="blue"  >
             <span style={{color: 'blue'}} >Blue</span>
         </Gamecircle>
         
-        <Gamecircle id ={5} value={7} >
+        <Gamecircle id ={5} color="red"  >
             <span style={{color: 'red'}} >Red</span>
         </Gamecircle>
         
-        <Gamecircle id ={6} value={7} >
+        <Gamecircle id ={6} color="blue"  >
             <span style={{color: 'blue'}} >Blue</span>
         </Gamecircle>
         
-        <Gamecircle id ={7} value={7} >
+        <Gamecircle id ={7} color="red"  >
             <span style={{color: 'red'}} >Red</span>
         </Gamecircle>
         
-        <Gamecircle id ={8} value={7} >
+        <Gamecircle id ={8} color="blue"  >
             <span style={{color: 'blue'}} >Blue</span>
         </Gamecircle>
         
--- src/Components/Gamecircle.js
+++ src/Components/Gamecircle.js
@@ -1,14 +1,14 @@
 import React from 'react'
 
-const onclick = (event, id, value) => {
+const onclick = (event, id, color) => {
   debugger;
   alert('onClick' + id);
 }
 
-const Gamecircle = ({id, value, children}) => {
+const Gamecircle = ({id, color, children}) => {
   console.log(id);
   return (
-    <div onClick={(event) => onclick(event, id, value) }>
+    <div style={{backgroundColor: color}} onClick={(event) => onclick(event, id, color) }>
       {/* Gamecircle - id= {id} */}
 
       {children}
