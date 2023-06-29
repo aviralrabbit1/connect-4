7188b646dbfb660693a25e2e42df534f8426c3b2
aviralrabbit1 <aviral.robin@gmail.com>
2023-06-30 04:48:53 +0530

    added circle colour children

--- src/Components/Gameboard.js
+++ src/Components/Gameboard.js
@@ -4,21 +4,37 @@ import Gamecircle from './Gamecircle'
 function Gameboard() {
   return (
     <div>
-        <Gamecircle id ={1} />
+        <Gamecircle id ={1} >
+            <div>Red</div>
+        </Gamecircle>
         
-        <Gamecircle id ={2} />
+        <Gamecircle id ={2} >
+            <div>Blue</div>
+        </Gamecircle>
         
-        <Gamecircle id ={3} />
+        <Gamecircle id ={3} >
+            <div>Red</div>
+        </Gamecircle>
         
-        <Gamecircle id ={4} />
+        <Gamecircle id ={4} >
+            <div>Blue</div>
+        </Gamecircle>
         
-        <Gamecircle id ={5} />
+        <Gamecircle id ={5} >
+            <div>Red</div>
+        </Gamecircle>
         
-        <Gamecircle id ={6} />
+        <Gamecircle id ={6} >
+            <div>Blue</div>
+        </Gamecircle>
         
-        <Gamecircle id ={7} />
+        <Gamecircle id ={7} >
+            <div>Red</div>
+        </Gamecircle>
         
-        <Gamecircle id ={8} />
+        <Gamecircle id ={8} >
+            <div>Blue</div>
+        </Gamecircle>
         
     </div>
   )
--- src/Components/Gamecircle.js
+++ src/Components/Gamecircle.js
@@ -4,11 +4,13 @@ const onclick = () => {
   alert('onClick');
 }
 
-const Gamecircle = ({id}) => {
+const Gamecircle = ({id, children}) => {
   console.log(id);
   return (
     <div onClick={onclick}>
       Gamecircle - id= {id}
+
+      {children}
     </div>
   )
 }
