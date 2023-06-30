c3cb7b9cac1d1ade5aa05d727b9efaaa294feac8
aviralrabbit1 <aviral.robin@gmail.com>
2023-06-30 05:48:03 +0530

    morphed into circle shape

--- src/Components/Gameboard.js
+++ src/Components/Gameboard.js
@@ -3,37 +3,69 @@ import Gamecircle from './Gamecircle'
 
 function Gameboard() {
   return (
-    <div>
+    <div style={{display: 'grid', gridTemplateColumns: '1fr 1fr 1fr 1fr', gridTemplateRows: '1fr 1fr 1fr 1fr', padding: 20}}>
         <Gamecircle id ={1} color="red"  >
-            <span style={{color: 'red'}} >Red</span>
+            {/* <span style={{color: 'red'}} >Red</span> */}
         </Gamecircle>
         
         <Gamecircle id ={2} color="blue"  >
-            <span style={{color: 'blue'}} >Blue</span>
+            {/* <span style={{color: 'blue'}} >Blue</span> */}
         </Gamecircle>
         
         <Gamecircle id ={3} color="red"  >
-            <span style={{color: 'red'}} >Red</span>
+            {/* <span style={{color: 'red'}} >Red</span> */}
         </Gamecircle>
 
         <Gamecircle id ={4} color="blue"  >
-            <span style={{color: 'blue'}} >Blue</span>
+            {/* <span style={{color: 'blue'}} >Blue</span> */}
         </Gamecircle>
         
         <Gamecircle id ={5} color="red"  >
-            <span style={{color: 'red'}} >Red</span>
+            {/* <span style={{color: 'red'}} >Red</span> */}
         </Gamecircle>
         
         <Gamecircle id ={6} color="blue"  >
-            <span style={{color: 'blue'}} >Blue</span>
+            {/* <span style={{color: 'blue'}} >Blue</span> */}
         </Gamecircle>
         
         <Gamecircle id ={7} color="red"  >
-            <span style={{color: 'red'}} >Red</span>
+            {/* <span style={{color: 'red'}} >Red</span> */}
         </Gamecircle>
         
         <Gamecircle id ={8} color="blue"  >
-            <span style={{color: 'blue'}} >Blue</span>
+            {/* <span style={{color: 'blue'}} >Blue</span> */}
+        </Gamecircle>
+
+        <Gamecircle id ={9} color="red"  >
+            {/* <span style={{color: 'red'}} >Red</span> */}
+        </Gamecircle>
+        
+        <Gamecircle id ={10} color="blue"  >
+            {/* <span style={{color: 'blue'}} >Blue</span> */}
+        </Gamecircle>
+        
+        <Gamecircle id ={11} color="red"  >
+            {/* <span style={{color: 'red'}} >Red</span> */}
+        </Gamecircle>
+
+        <Gamecircle id ={12} color="blue"  >
+            {/* <span style={{color: 'blue'}} >Blue</span> */}
+        </Gamecircle>
+        
+        <Gamecircle id ={13} color="red"  >
+            {/* <span style={{color: 'red'}} >Red</span> */}
+        </Gamecircle>
+        
+        <Gamecircle id ={14} color="blue"  >
+            {/* <span style={{color: 'blue'}} >Blue</span> */}
+        </Gamecircle>
+        
+        <Gamecircle id ={15} color="red"  >
+            {/* <span style={{color: 'red'}} >Red</span> */}
+        </Gamecircle>
+        
+        <Gamecircle id ={16} color="blue"  >
+            {/* <span style={{color: 'blue'}} >Blue</span> */}
         </Gamecircle>
         
     </div>
--- src/Components/Gamecircle.js
+++ src/Components/Gamecircle.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const onclick = (event, id, color) => {
+const onclick = (event, id) => {
   debugger;
   alert('onClick' + id);
 }
@@ -8,7 +8,7 @@ const onclick = (event, id, color) => {
 const Gamecircle = ({id, color, children}) => {
   console.log(id);
   return (
-    <div style={{backgroundColor: color}} onClick={(event) => onclick(event, id, color) }>
+    <div style={{backgroundColor: color, width: 100, height: 100, margin: 10, borderRadius: '50%'}} onClick={(event) => onclick(event, id) }>
       {/* Gamecircle - id= {id} */}
 
       {children}
